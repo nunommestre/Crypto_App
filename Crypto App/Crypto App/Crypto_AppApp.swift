ce1eb1f139c6c8dad69e39ffa65458039945db83
Nuno Mestre <nuno_mestre@student.uml.edu>
2022-08-12 18:57:52 -0400

    add coin data from API & networking layer

--- Crypto App/Crypto App/Crypto_AppApp.swift
+++ Crypto App/Crypto App/Crypto_AppApp.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 @main
 struct Crypto_AppApp: App {
+  
+  @StateObject private var vm = HomeViewModel()
     var body: some Scene {
         WindowGroup {
           NavigationView{
             HomeView()
               .navigationBarHidden(true)
           }
+          .environmentObject(vm)
         }
     }
 }
